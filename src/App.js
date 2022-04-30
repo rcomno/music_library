2c0f78dd0589118b30a55b6bc76fd744f2a07111
Rocco Mannino <RWMannio@gmail.com>
2022-04-30 02:04:45 -0400

    Branch 2

--- src/App.js
+++ src/App.js
@@ -1,34 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect ,useState } from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Gallery from "./components/Gallery";
 import SearchBar from "./components/SearchBar";
+import ArtistView from './components/ArtistView';
+import AlbumView from './components/AlbumView';
 
 function App() {
   let [search, setSearch] = useState("")
   let [message, setMessage] = useState("Search for Music!")
   let [data, setData] = useState([])
 
+  const API_URL = 'https://itunes.apple.com/search?term='
+
   useEffect(() => {
-    const fetchData = async() => {
-      document.title = `${search} Music`
-      const response = await fetch('https://itunes.apple.com/search?term=the%20grateful%20dead')
-      const resData = await response.json()
-      if (resData.results.length > 0) {
-        setData(resData.results)
-      } else {
-        setMessage('NotFound')
-      }
-      console.log(resData)
-    } 
-    fetchData()
+    if (search) {
+      const fetchData = async() => {
+        document.title = `${search} Music`
+        const response = await fetch(API_URL + search)
+        const resData = await response.json()
+        if (resData.results.length > 0) {
+          setData(resData.results)
+        } else {
+          setMessage('NotFound')
+        }
+        console.log(resData)
+      } 
+      fetchData()
+    }
   }, [search])
+
+  const handleSearch = (e, term) => {
+    e.preventDefault()
+    setSearch(term)
+  }
 
   return (
     <div className="App">
-      <SearchBar />
       {message}
-      <Gallery />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Fragment><SearchBar handleSearch={handleSearch} /><Gallery data={data} /></Fragment> } />
+          <Route path="/album/:id" element={<AlbumView />} />
+          <Route path="/artist/:id" element={<ArtistView />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
